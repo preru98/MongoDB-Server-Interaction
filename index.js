97cdea50668addd0bd63f455d998bec1fc8c692c
Prerna <prerna1998.ps@gmail.com>
2020-07-18 02:06:52 +0530

    Callback Hell | Promises

--- index.js
+++ index.js
@@ -6,30 +6,38 @@ const dbOperations = require('./operations')
 const url = 'mongodb://localhost:27017/'
 const dbName='conFusion'
 
-MongoClient.connect(url, (err, client)=>{
-    assert.equal(err, null)
+MongoClient.connect(url)
+.then((client) => {
+
     console.log('Connected')
     const db = client.db(dbName) 
     
-    dbOperations.insertDocument(db, { name: "test-name", description: "test-description"}, "dishes", (result) => {
-        console.log("Insert Document:\n", result) 
+    dbOperations.insertDocument(db, { name: "test-name", description: "test-description"}, "dishes")
+    .then((result) => {
+        console.log("Insert Document:\n", result)
 
-        dbOperations.findDocuments(db, "dishes", (docs) => {
-            console.log("Found Documents:\n", docs) 
-
-            dbOperations.updateDocument(db, { name: "test-name" },{ description: "updated-test-description"}, "dishes", (result) => {
-                console.log("Updated Document:\n", result.result) 
-
-                dbOperations.findDocuments(db, "dishes", (docs) => {
-                    console.log("Found Updated Documents:\n", docs) 
-                    
-                    db.dropCollection("dishes", (result) => {
-                        console.log("Dropped Collection: ", result) 
-
-                        client.close()
-                    })
-                })
-            })
-        })
+        return dbOperations.findDocuments(db, "dishes")
     })
+    .then((docs) => {
+        console.log("Found Documents:\n", docs) 
+        
+        return dbOperations.updateDocument(db, { name: "test-name" },{ description: "updated-test-description"}, "dishes")
+    })
+    .then((result) => {
+        console.log("Updated Document:\n", result.result) 
+
+       return dbOperations.findDocuments(db, "dishes")
+    })
+    .then((docs) => {
+        console.log("Found Updated Documents:\n", docs) 
+                    
+        return db.dropCollection("dishes")
+    })
+    .then((result) => {
+        console.log("Dropped Collection: ", result) 
+        client.close()
+    })
+})
+.catch( (err) => {
+    console.log(err)
 })
--- operations.js
+++ operations.js
@@ -2,35 +2,21 @@ const assert =require('assert')
 
 exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection)
-    coll.insert(document, (err, result)=>{
-        assert.equal(err, null)
-        console.log("Inserted" , result.result.n + " document(s)")
-        callback(result)
-    })
+    return coll.insert(document)
 }
 
 exports.findDocuments = (db, collection, callback) => {
     const coll = db.collection(collection)
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null)
-        callback(docs)
-    })  
+    return coll.find({}).toArray()  
 }
 
 exports.removeDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection)
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null)
-        callback(docs)
-    })  
+    return coll.deleteOne(document)
 }
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection)
-    coll.updateOne(document, {$set : update}, null, (err,result) => {
-        assert.equal(err, null)
-        console.log("updated the document ", update)
-        callback(result)
-    })
+    return coll.updateOne(document, {$set : update}, null)
 }
 
